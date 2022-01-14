de1a0cf870ecfe1dde6510e002b2af566db68f90
Sambit Mishra <sambitm@twitter.com>
2022-01-13 20:33:03 -0800

    Code refactored.README modified.

--- Assignment.java
+++ Assignment.java
@@ -34,33 +34,59 @@ public class Assignment {
 			}
 
 		}
+
 	}
 
 	public static void main(String[] args) {
 
-		Thread t1 = new Thread(new ParallelTask("group1"), "Thread - T1");
-		Thread t2 = new Thread(new ParallelTask("group2"), "Thread - T2");
-		Thread t3 = new Thread(new ParallelTask("group3"), "Thread - T3");
+		Thread t1 = new Thread(new ParallelTask("group1"));
+		Thread t2 = new Thread(new ParallelTask("group2"));
+		Thread t3 = new Thread(new ParallelTask("group3"));
 
-		File folder = new File("./");
-
-		for (File fileEntry : folder.listFiles()) {
-			if(!processedFileSet.contains(fileEntry.getName())) {
-				processedFileSet.add(fileEntry.getName());
-				if (fileEntry.getName().startsWith("group1")) {
-					group1Queue.offer(fileEntry.getName());
-				} else if (fileEntry.getName().startsWith("group2")) {
-					group2Queue.offer(fileEntry.getName());
-				} else if (fileEntry.getName().startsWith("group3")) {
-					group3Queue.offer(fileEntry.getName());
-				}
-			}
-		}
+		// Populate the group queues based on the initial contents of the directory
+		populateQueue();
 
 		t1.start();
 		t2.start();
 		t3.start();
+
+		try {
+			/***
+			 * This is a continuous running process , so that if we keep adding files to the directory, it will
+			 * be processed. The program exits if there is an exception or the user forcefully exits it.
+			 * The main thread sleeps for 60 seconds between loops.
+			 */
+			while (true) {
+
+				Thread.sleep(60000);
+				populateQueue();
+
+				t1.join();
+				t1 = new Thread(new ParallelTask("group1"));
+				t1.start();
+
+				t2.join();
+				t2 = new Thread(new ParallelTask("group2"));
+				t2.start();
+
+				t3.join();
+				t3 = new Thread(new ParallelTask("group3"));
+				t3.start();
+			}
+		}catch (Exception ex){
+			System.out.println("Exception Caught - Exiting: " + ex.getMessage());
+		}
 	}
+
+	/***
+	 *
+	 * Method which reads the file from the disk and creates a .out file. We load a single line onto to memory
+	 * and the write it to the output file. We do not load the entire file into the memory. This can help to process
+	 * larger files which likely not fit in the memory
+	 *
+	 * @param source Path of the Input .txt file
+	 * @param destination Path of the processed .out file
+	 */
 	public static void readAndWriteFile(String source, String destination){
 		try {
 			BufferedWriter buf = new BufferedWriter(new FileWriter(destination, true));
@@ -71,8 +97,28 @@ public class Assignment {
 				buf.newLine();
 			}
 			buf.close();
+			System.out.println("Created: " + destination + " for " + source);
 		}catch (Exception ex){
 			System.out.println(ex.getMessage());
+		}
+	}
+
+	/***
+	 * Method which populates the specific Queues for each group of files
+	 */
+	public static void populateQueue() {
+		File folder = new File("./");
+		for (File fileEntry : folder.listFiles()) {
+			if(!processedFileSet.contains(fileEntry.getName()) && fileEntry.getName().endsWith(".txt")) {
+				processedFileSet.add(fileEntry.getName());
+				if (fileEntry.getName().startsWith("group1")) {
+					group1Queue.offer(fileEntry.getName());
+				} else if (fileEntry.getName().startsWith("group2")) {
+					group2Queue.offer(fileEntry.getName());
+				} else if (fileEntry.getName().startsWith("group3")) {
+					group3Queue.offer(fileEntry.getName());
+				}
+			}
 		}
 	}
 }
--- README.md
+++ README.md
@@ -2,9 +2,16 @@
 Process Files in a Directory
 
 This helps to process files in the current directory and apply a Lower Case Transformation to the contents of the file.
-Once the transformation is applied an output file is created with a .out extension.
+Once the transformation is applied an output file is created with a .out extension. This is a continuous running process and
+new files can be added to the directory as the process is run.
+
+The program exits if there is an Exception or the user exits is forcefully
 
 # Steps to Execute the Program
+
+**Prerequisites**
+
+The code can be executed on any Linux/Mac/Windows OS running JDK 6 and above
 
 ```
 1 - git clone https://github.com/04msambit/FileProcessor
@@ -14,4 +21,17 @@ Once the transformation is applied an output file is created with a .out extensi
 ```
 
 Once the above is executed the .out files will be created in the directory
+
+# Program Logic
+
+Since we have been provided with 3 groups of files, we have created a Thread Pool of size 3 which applies the transformation on each of the groups
+We have used 3 separate queues to store the names of the files. Each Thread process a separate queue.
+
+# Tests Carried Out
+
+1 - Add 3 text files of each group in the folder
+2 - Start the process
+3 - Verify the .out files are created in the same folder
+4 - Add new files while the process is running
+5 - The .out files corresponding to new files should be created
 
